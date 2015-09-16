41e3e532b2f7da9fc626143d5484cb399a3a7a19
Michael Mellinger <mmellinger66@gmail.com>
2015-09-16 15:10:14 -0400

    More urls

--- bin/crawl.sh
+++ bin/crawl.sh
@@ -4,31 +4,59 @@
 import Darwin
 import Foundation
 
+func crawlSite2(urlPath: String) -> String {
 
-func crawlSite(urlPath: String) -> String {
+let url = NSURL(string: urlPath)
+let request = NSURLRequest(URL: url!, cachePolicy: .ReloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 5.0)
+//var htmlString:String = ""
+var x:String = "z"
 
-    var url: NSURL = NSURL(string: urlPath)!
-    var request1: NSURLRequest = NSURLRequest(URL: url)
-    var response: AutoreleasingUnsafeMutablePointer<NSURLResponse?> = nil
-    var error: NSErrorPointer = nil
-    var data: NSData =  NSURLConnection.sendSynchronousRequest(request1, returningResponse: response, error:nil)!
-    let aString = NSString(data: data, encoding: NSUTF8StringEncoding)
-    let htmlDocument:String = aString as String
+let session = NSURLSession.sharedSession()
 
-    return htmlDocument
+session.dataTaskWithRequest(request, completionHandler: {(data, response, error) in
+    print(data)
+    // html = data as! String
+      if let htmlString = NSString(data: data!, encoding:NSUTF8StringEncoding) {
+        x = htmlString as String
+      } else {
+        x = "y"
+
+
+      }
+        print(response)
+	    print(error)
+    }).resume()
+print(x)
+    return x
 }
+
+//func crawlSite(urlPath: String) -> String {
+//
+//    var url: NSURL = NSURL(string: urlPath)!
+//    var request1: NSURLRequest = NSURLRequest(URL: url)
+//    var response: AutoreleasingUnsafeMutablePointer<NSURLResponse?> = nil
+//    var error: NSErrorPointer = nil
+//    var data: NSData =  NSURLConnection.sendSynchronousRequest(request1, returningResponse: response, error:nil)!
+//    let aString = NSString(data: data, encoding: NSUTF8StringEncoding)
+//    let htmlDocument:String = aString as! String
+//
+//    return htmlDocument
+//}
 
 mkdir("./data", 511)
 
 let url = "http://google.com"
 mkdir("./data", 511)
 
-let htmlDocument = crawlSite(url)
+let htmlDocument = crawlSite2(url)
 let f0 = url.componentsSeparatedByString("//")
 let url0:String = f0[1]
 
 let filePath = "data/" + url0
     
 //println("\(htmlDocument)")
-htmlDocument.writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding, error: nil)
-
+ do {
+try htmlDocument.writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding)
+} catch {
+    print("ERROR: Writing file")
+}
